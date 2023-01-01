d8f9f6bdac5224c4c235a90260188805aa0b4b99
Nofarya <94351478+Nofarya@users.noreply.github.com>
2023-01-01 19:37:01 +0200

    updating before pulling the latest version

---
+++ src/LiveModel.py
@@ -0,0 +1,29 @@
+# Importing the required packages
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from Model import svc, DecisionTree
+
+# Put live data as the test
+X_test, y_test =
+
+# SVC Prediction
+y_pred = svc.predict(X_test)
+print("Predicted values:")
+print(y_pred)
+
+# Prediction Factors
+print("Confusion Matrix: ", confusion_matrix(y_test, y_pred))
+print("Accuracy : ", accuracy_score(y_test, y_pred) * 100)
+print("Report : ", classification_report(y_test, y_pred))
+
+
+# Decision Tree Prediction
+y_pred = DecisionTree.predict(X_test)
+print("Predicted values:")
+print(y_pred)
+
+# Prediction Factors
+print("Confusion Matrix: ", confusion_matrix(y_test, y_pred))
+print("Accuracy : ", accuracy_score(y_test, y_pred) * 100)
+print("Report : ", classification_report(y_test, y_pred))
