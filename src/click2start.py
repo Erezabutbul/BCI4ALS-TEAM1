2241f22744998002fbb297de5615f8fb895c5053
Erezabutbul <textours@gmail.com>
2023-03-09 16:35:03 +0200

    click2start.py is set for test/train states click2test.py & GUIForTest.py are removed

--- src/click2start.py
+++ src/click2start.py
@@ -11,29 +11,35 @@ from data_extraction_by_class import main as data_spliting_main
 from psycho_data_Arrange import main as arrange_markers_main
 from avgData import main as avgData_main
 from featureExtraction import main as featureExtraction_main
+from featureExtractionForTest import main as featureExtractionForTest_main
 from Model import main as modelToTrain
+from LiveModel import main as liveModelPrediction
 
 
-
-def createFile():
+def createFile(state):
     # date & time
     date = datetime.now().strftime("%d_%m_%Y at %I_%M_%S_%p")
 
     # Create the "EXP_{date}" directory
-    exp_dir = f"output_files/EXP_{date}/"
+    if state == "train":
+        exp_dir = f"output_files/EXP_{date}/"
+    else:
+        exp_dir = f"output_files/testSet/test_{date}/"
     os.makedirs(exp_dir, exist_ok=True)
     return exp_dir
 
 
 def main():
+    # state of EXP : train or test
+    state = "train"
     # Create the "EXP_{date}" directory
-    exp_path = createFile()
+    exp_path = createFile(state)
 
     with multiprocessing.Manager() as manager:
         keepRunning = manager.Value('b', True)
         # Create the processes
         p1 = multiprocessing.Process(target=lsl_main, args=[exp_path, keepRunning])
-        p2 = multiprocessing.Process(target=showExperiment, args=[exp_path, keepRunning])
+        p2 = multiprocessing.Process(target=showExperiment, args=[exp_path, keepRunning, state])
 
         # Start the processes
         p1.start()
@@ -60,10 +66,10 @@ def main():
     avgData_main(exp_path)
 
     # extract features
-    featureExtraction_main(exp_path)
+    featureExtraction_main(exp_path, state)
 
-    # train model
-    modelToTrain(exp_path)
+    # TODO - convert Model & LiveModel files into handleModel as discussed
+    # handleModel(exp_path, state)
 
 
 if __name__ == '__main__':
