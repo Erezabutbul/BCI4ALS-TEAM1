e65375ff92977f05246f09a7b5957ab52fd2232a
Nofarya <94351478+Nofarya@users.noreply.github.com>
2023-03-11 09:31:07 +0200

    updating before pulling the latest version

--- src/DataByFeature.py
+++
@@ -1,9 +0,0 @@
-# Importing the required packages
-import pandas as pd
-from FeatureExtraction import Amplitude, Slope, Latency, PeakWidth
-
-
-fueatures = {'Amplitude': Amplitude, 'Slope':  Slope, 'Latency':  Latency, 'Peak width':  PeakWidth}
-X = pd.DataFrame(data=fueatures)
-print(X)
-
